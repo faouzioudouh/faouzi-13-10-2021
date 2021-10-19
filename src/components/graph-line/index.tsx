4a4f9a80305463dd0612555e71df2efa838d896c
Faouzi OUDOUH <faouzi.oudouh@gmail.com>
2021-10-19 17:57:48 +0200

    fix svg

--- src/components/graph-line/index.tsx
+++ src/components/graph-line/index.tsx
@@ -10,7 +10,7 @@ interface Props {
 export const GraphLine: VFC<Props> = ({ width, theme }) => {
   return (
     <svg className="graph-line">
-      <rect x="0" y="0" width={`${width}%`} height="100%" fill={theme} fill-opacity="0.2"></rect>
+      <rect x="0" y="0" width={`${width}%`} height="100%" fill={theme} fillOpacity="0.2"></rect>
     </svg>
   );
 };
